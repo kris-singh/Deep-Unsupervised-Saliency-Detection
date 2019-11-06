c4d1c906bc94dda929e2da3ec0e86eb6d0256301
kris-singh <krishnakant.singh@suiit.ac.in>
2019-11-06 09:03:28 +0000

    Breaking Change: Naming Convention Change for configs

--- src/config.py
+++ src/config.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+#!/usr/bin/env python
+
 from yacs.config import CfgNode as CN
 
 
@@ -16,40 +18,56 @@ _C.SYSTEM.WAIT_TIME = 10
 _C.SYSTEM.CHKPT_FREQ = 5
 _C.SYSTEM.LOG_FREQ = 10
 _C.SYSTEM.EXP_NAME = 'default'
+_C.SYSTEM.EXP_TYPE = 'full'
+_C.SYSTEM.SEED = 43
 
-# Training setting
-_C.TRAIN = CN()
-_C.TRAIN.SEED = 7
-_C.TRAIN.ROOT = '/data/workspaces/krish/sd/msrab_hkuis/'
-_C.TRAIN.NOISE_ROOT = ('../noisy_gt/noisy_gt_1', '../noisy_gt/noisy_gt_2', '../noisy_gt/noisy_gt_3')
-_C.TRAIN.LIST = '/data/workspaces/krish/sd/msrab_hkuis/small.lst'
-_C.TRAIN.LR = 1e-3
-_C.TRAIN.LAMBDA = 0.01
-_C.TRAIN.MOMENTUM = 0.9
-_C.TRAIN.EPOCHS = 20
-_C.TRAIN.OPTIMIZER = 'ADAM'
-_C.TRAIN.BETAS = (0.9, 0.99)
-_C.TRAIN.IMG_SIZE = (256, 256)
-_C.TRAIN.ALPHA = 0.01
-_C.TRAIN.BATCH_SIZE = 2
-_C.TRAIN.NUM_MAPS = 3
-_C.TRAIN.NUM_IMG = 3000
-_C.TRAIN.SAVE_ROOT = '/data/workspaces/krish/sd/experiments'
+# Dataset setting
+_C.DATA = CN()
+_C.DATA.TRAIN = CN()
+_C.DATA.TRAIN.ROOT = '/data/workspaces/krish/sd/msrab_hkuis/'
+_C.DATA.TRAIN.NOISE_ROOT = ('../noisy_gt/noisy_gt_1', '../noisy_gt/noisy_gt_2', '../noisy_gt/noisy_gt_3')
+_C.DATA.TRAIN.LIST = '/data/workspaces/krish/sd/msrab_hkuis/small.lst'
+_C.DATA.VAL = CN()
+_C.DATA.VAL.ROOT = '/data/workspaces/krish/sd/msrab_hkuis/'
+_C.DATA.VAL.LIST = '/data/workspaces/krish/sd/msrab_hkuis/small_val.lst'
+_C.DATA.TEST = CN()
+_C.DATA.TEST.ROOT = '/data/workspaces/krish/sd/msrab_hkuis/'
+_C.DATA.TEST.LIST = '/data/workspaces/krish/sd/msrab_hkuis/small_test.lst'
+
+
+# Optimizer
+_C.SOLVER = CN()
+_C.SOLVER.OPTIMIZER = 'SGD'
+_C.SOLVER.SCHEDULER = 'ReduceLROnPlateu'
+_C.SOLVER.LAMBDA = 0.01
+_C.SOLVER.EPOCHS = 20
+_C.SOLVER.IMG_SIZE = (256, 256)
+_C.SOLVER.ALPHA = 0.01
+# Optimizer Settings
+_C.SOLVER.BETAS = (0.9, 0.99)
+_C.SOLVER.MOMENTUM = 0.9
+_C.SOLVER.LR = 1e-3
+_C.SOLVER.WEIGHT_DECAY = 0.0
+# Scheduler Settings
+_C.SOLVER.STEP_SIZE = 10
+_C.SOLVER.FACTOR = 0.9
+_C.SOLVER.MIN_LR = 1e-12
+_C.SOLVER.PAITENCE = 10
+_C.SOLVER.THRESHOLD = 1e-6
+_C.SOLVER.COOLDOWN = 1
+_C.SOLVER.BATCH_SIZE = 2
+_C.SOLVER.NUM_MAPS = 3
+_C.SOLVER.NUM_IMG = 3000
 
 # Validation Setting
 _C.VAL = CN()
-_C.VAL.ROOT = '/data/workspaces/krish/sd/msrab_hkuis/'
-_C.VAL.LIST = '/data/workspaces/krish/sd/msrab_hkuis/small_val.lst'
-_C.VAL.BATCH_SIZE = 5
 # TEST Setting
 _C.TEST = CN()
-_C.TEST.ROOT = '/data/workspaces/krish/sd/msrab_hkuis/'
-_C.TEST.LIST = '/data/workspaces/krish/sd/msrab_hkuis/small_test.lst'
-_C.TEST.BATCH_SIZE = 5
-
 # Noise
 _C.NOISE = CN()
-_C.NOISE.ALPHA=0.01
+_C.NOISE.ALPHA = 0.01
 
+# Miscellaneous
+_C.SAVE_ROOT = '/data/workspaces/krish/sd/experiments'
 
 cfg = _C  # users can `from config import cfg`
