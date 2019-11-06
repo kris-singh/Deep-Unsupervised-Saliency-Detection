6bd0cebbcaaca49914e13bafe44bc317dbed9f93
kris-singh <krishnakant.singh@suiit.ac.in>
2019-11-06 08:57:54 +0000

    Major Fix: Change Logic of Early Stopping(Maybe make this merge with early stopping commit(earliest)

--- src/utils/early_stopping.py
+++ src/utils/early_stopping.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-import torch
 import logging
 
 import torch
@@ -8,33 +7,54 @@ from utils.meter import MetricLogger
 from utils.metrics import log_metrics
 
 
-def check_cgt(cfg, model, val_loader, epoch_idx, wait, val_losses):
-    logger = logging.getLogger(cfg.SYSTEM.EXP_NAME)
-    wait_period = cfg.SYSTEM.WAIT_TIME
-    cgt = 0
-    loss = validate(cfg, model, val_loader, epoch_idx, logger)
-    val_losses.append(loss.detach().cpu())
-    if len(val_losses) > 2 and abs(val_losses[-1]-val_losses[-2]) <= 1e-4:
-        print(val_losses[-1]-val_losses[-2])
-        if wait > wait_period:
-            cgt, wait = 1, 0
+class EarlyStopping:
+    def __init__(self, model, val_loader, cfg):
+        self.model = model
+        self.val_loader = val_loader
+        self.best_f1 = None
+        self.cfg = cfg
+        self.logger = logging.getLogger(str(cfg.SYSTEM.EXP_NAME) + '.utils.early_stopping')
+        self.paitence = cfg.SYSTEM.WAIT_TIME
+        self.wait = 0
+        self.f1 = None
+        self.converged = False
+        self.val_loss = MetricLogger()
+
+    def reset(self):
+        self.converged = False
+        self.wait = 0
+
+    def check_cgt(self):
+        if self.best_f1 is None:
+            self.best_f1 = self.f1
+        elif self.best_f1 < self.f1:
+            if self.wait > self.paitence:
+                self.converged = True
+                self.wait = 0
+            else:
+                self.wait += 1
         else:
-            wait = wait+1
-            cgt, wait = cgt, wait
-    else:
-        cgt, wait = 0, wait
-    return cgt, wait
+            self.wait = 0
+            self.best_f1 = self.f1
 
+    def validate(self, writer, epoch_idx):
+        self.model.eval()
+        pred_loss = torch.nn.BCEWithLogitsLoss()
+        metrics = None
+        for idx, data in enumerate(self.val_loader):
+            x = data['sal_image'].to(self.cfg.SYSTEM.DEVICE)
+            y = data['sal_label'].to(self.cfg.SYSTEM.DEVICE)
+            y_pred = self.model(x)['out']
+            loss = pred_loss(y_pred, y) / self.cfg.SOLVER.BATCH_SIZE
+            self.val_loss.update(**{'val_loss': loss})
+            if idx % self.cfg.SYSTEM.LOG_FREQ:
+                self.logger.debug(f'Validation Loss: {loss}, Avg: {self.val_loss.meters["val_loss"].avg}')
 
-def validate(cfg, model, val_loader, epoch, logger):
-    model.eval()
-    pred_loss = torch.nn.BCEWithLogitsLoss()
-    losses = []
-    for idx, data in enumerate(val_loader):
-        x = data['sal_image'].to(cfg.SYSTEM.DEVICE)
-        y = data['sal_label'].to(cfg.SYSTEM.DEVICE)
-        y_pred = model(x)['out']
-        losses.append((pred_loss(y_pred, y)))
-
-    logger.debug(f'Validation Loss: loss={sum(losses)/len(losses)}')
-    return sum(losses)/len(losses)
+            metrics = log_metrics(y_pred, y)
+        self.f1 = 1. / (metrics.meters['precision'].avg+1e-16) + 1. / (metrics.meters['recall'].avg+1e-16)
+        writer.add_scalars('val_metrics', {'val_precision': metrics.meters['precision'].avg,
+                                            'val_recall': metrics.meters['recall'].avg,
+                                            'val_f1': self.f1,
+                                            'val_mae': metrics.meters['mae'].avg}, epoch_idx)
+        writer.add_scalar('val_loss', loss, epoch_idx)
+        self.check_cgt()
