84d5fdeb1365fa000ea6ff213d44de32656f08a5
kris-singh <krishnakant.singh@suiit.ac.in>
2019-11-06 08:18:55 +0000

    Add: set seed and custom lr scheduler fnc

--- src/utils/basic.py
+++ src/utils/basic.py
@@ -15,3 +15,15 @@ def count_nonzero(x):
     x: np.array of shape (1, ) or (2, ?)
     """
     return len(np.transpose(np.nonzero(x)))
+
+
+def set_seeds(cfg):
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    torch.manual_seed(cfg.SYSTEM.SEED)
+    np.random.seed(cfg.SYSTEM.SEED)
+
+
+def custom_lr(base_lr, maxiter, power):
+    fnc = lambda iter: base_lr * (1 - iter / maxiter) ** power
+    return fnc
